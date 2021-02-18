b645277f479adf779e245c378e603368928d6298
gray <gray@2b.lan>
2021-02-17 17:19:30 -0800

    fix wrong directory bug

--- enable_dd.sh
+++ enable_dd.sh
@@ -9,6 +9,7 @@ pip install --upgrade pip
 pip install tensorflow
 
 # fetch pretrained model
+cd ..
 wget https://github.com/KichangKim/DeepDanbooru/releases/download/v4-20200814-sgd-e30/deepdanbooru-v4-20200814-sgd-e30.zip
 unzip deepdanbooru-v4-20200814-sgd-e30.zip -d dd_pretrained
 rm deepdanbooru-v4-20200814-sgd-e30.zip
